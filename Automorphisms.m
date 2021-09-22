c4978023209e2a3cecf55b2d0ea1d36bc896f1c0
grydquist <grydquist@outlook.com>
2021-09-22 14:57:13 -0400

    Added gif, made things look a bit nicer

--- Automorphisms.m
+++ Automorphisms.m
@@ -55,11 +55,17 @@ scatter(p2(1,:),p2(2,:))
 scatter(p3(1,:),p3(2,:))
 
 %% Try by just using nearest lattice point
+h = figure;
+axis tight manual % this ensures that getframe() returns a consistent size
+filename = 'periodic.gif';
+
 % This really seems to work?????
 L2 = L;
-mdot = [1;0];
+LL = L;
+mdot = [0.35;0.35];
+mdot2 = mdot.*1.5;
 % Move the lattice a little each time
-maxt = 250;
+maxt = 100;
 theta = linspace(1,20,maxt);
 uu = zeros(2,2,maxt);
 uu(1,1,:) = sin(theta);
@@ -69,8 +75,12 @@ uu(2,2,:) = -sin(theta);
 for t = 1:maxt
 dt = 0.01;
 A = uu(:,:,t);
+
 L2 = L2 + A*L2*dt;
+LL = LL + A*LL*dt;
 mdot = mdot + A*mdot*dt;
+mdot2 = mdot2 + A*mdot2*dt;
+
 d = max([norm(L2(:,1)),norm(L2(:,2))]);
         if t==20
             disp(1)
@@ -109,12 +119,38 @@ myp2 = ii2*[L2(1,1);L2(2,1)] + jj2*[L2(1,2);L2(2,2)];
 L2 = [myp,myp2];
 clf
 L3 = L2;%[myp';myp2'];
-plot([0,L3(1,1)],[0,L3(2,1)]);hold on;plot([0,L3(1,2)],[0,L3(2,2)])
-plot([0,L3(1,1)] + L3(1,2),[0,L3(2,1)] + L3(2,2) );hold on;plot([0,L3(1,2)]+L3(1,1),[0,L3(2,2)]+L3(2,1))
+plot([0,L3(1,1)],[0,L3(2,1)],'r');hold on;plot([0,L3(1,2)],[0,L3(2,2)],'r')
+plot([0,L3(1,1)] + L3(1,2),[0,L3(2,1)] + L3(2,2),'r');hold on;plot([0,L3(1,2)]+L3(1,1),[0,L3(2,2)]+L3(2,1),'r')
+
+plot([0,LL(1,1)],[0,LL(2,1)],'b');hold on;plot([0,LL(1,2)],[0,LL(2,2)],'b')
+plot([0,LL(1,1)] + LL(1,2),[0,LL(2,1)] + LL(2,2),'b');hold on;plot([0,LL(1,2)]+LL(1,1),[0,LL(2,2)]+LL(2,1),'b')
+
+
+for i = -5:5
+    for j = -5:5
+        it = it + 1;
+        plot(mdot(1) + i*LL(1,1) + j*LL(1,2),mdot(2) + i*LL(2,1) + j*LL(2,2),'ko');
+        plot(mdot2(1) + i*LL(1,1) + j*LL(1,2),mdot2(2) + i*LL(2,1) + j*LL(2,2),'go');
+        
+    end
+end
+% plot(mdot(1),mdot(2),'o');
+% plot(mdot2(1),mdot2(2),'o');
+
 axis([-2,2,-2,2])
-drawnow 
-pause(0.1);
+drawnow
+%       frame = getframe(h); 
+%       im = frame2im(frame); 
+%       [imind,cm] = rgb2ind(im,256); 
+%       % Write to the GIF File 
+%       if t == 1 ],'b')
+% plot([0,LL(1,1)] + LL(1,2),[0,LL(2,1)] + LL(2,2),'b');hold on;plot([0,
+%           imwrite(imind,cm,filename,'gif', 'Loopcount',inf); 
+%       else 
+%           imwrite(imind,cm,filename,'gif','WriteMode','append'); 
+%       end 
+% pause(0.1);
 end
 L3 = L2;%[myp';myp2'];
-plot([0,L3(1,1)],[0,L3(2,1)]);hold on;plot([0,L3(1,2)],[0,L3(2,2)])
-plot([0,L3(1,1)] + L3(1,2),[0,L3(2,1)] + L3(2,2) );hold on;plot([0,L3(1,2)]+L3(1,1),[0,L3(2,2)]+L3(2,1))
+% plot([0,L3(1,1)],[0,L3(2,1)]);hold on;plot([0,L3(1,2)],[0,L3(2,2)])
+% plot([0,L3(1,1)] + L3(1,2),[0,L3(2,1)] + L3(2,2) );hold on;plot([0,L3(1,2)]+L3(1,1),[0,L3(2,2)]+L3(2,1))
